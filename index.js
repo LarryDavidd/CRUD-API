08a43edf977cb0a5f055b3f599082a23b0d33523
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-02-10 21:53:55 +0400

    feat: create connect to server

---
+++ index.js
@@ -0,0 +1,18 @@
+import { createServer } from "http";
+import { routes } from "#app/router.js";
+
+const DOMAIN = "http://localhost";
+const PORT = 3000;
+const HOST = `${DOMAIN}:${PORT}`;
+
+const app = createServer((request, response) => {
+  const parsedUrl = new URL(request.url, HOST);
+  request.appParsedUrl = parsedUrl;
+
+  const reqFn =
+    routes[parsedUrl.pathname] ?? ((_, response) => response.end("404"));
+  reqFn(request, response);
+});
+
+app.listen(PORT);
+console.log("Server is active now");
